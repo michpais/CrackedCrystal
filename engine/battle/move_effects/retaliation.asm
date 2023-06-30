9d6df5316a58a4361aa236f50029ab899db9c99d
michpais <82790137+michpais@users.noreply.github.com>
2023-06-30 00:48:51 -0400

    Updated retalation to doubledamage at <1/2 health

--- engine/battle/move_effects/retaliation.asm
+++ engine/battle/move_effects/retaliation.asm
@@ -1,9 +1,14 @@
 BattleCommand_Retaliation:
-; Get the user's status condition
-	ld a, BATTLE_VARS_STATUS
-	call GetBattleVar
-; Return if it's 0 (no condition)
-	and a
-	ret z
-; It's not 0, so double the damage
+; Get the user's quarter of max HP
+	callfar GetHalfMaxHP
+	callfar CheckUserHasEnoughHP
+	jr c, .failed
 	jp DoubleDamage
+.failed
+	ret
+
+; Return if it's 0 (no condition)
+;	and a
+;	ret z
+; It's not 0, so double the damage
+;	jp DoubleDamage
