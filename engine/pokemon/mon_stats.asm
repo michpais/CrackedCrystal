25d9bc5c7a99459b491f7a9a0404b8e49a7efeae
Brianum <brian+github@saydak.de>
2025-04-29 08:18:10 +0200

    Rename and document ``BrokenPlacePPUnits`` (#1179)

--- engine/pokemon/mon_stats.asm
+++ engine/pokemon/mon_stats.asm
@@ -405,21 +405,47 @@ ListMovePP:
 	jr nz, .load_loop
 	ret
 
-;BrokenPlacePPUnits: ; unreferenced
-;; Probably would have these parameters:
-;; hl = starting coordinate
-;; de = SCREEN_WIDTH or SCREEN_WIDTH * 2
-;; c = the number of moves (1-4)
-;.loop
-;	ld [hl], $32 ; typo for P?
+;; "AP" is german for "PP"
+;; The german translation uses this instead of the loop above
+;; Basically, we don't need this as CrackedCrystal only supports English
+;.load_ap_loop ; unreferenced
+;	ld [hl], $32 ; A
 ;	inc hl
 ;	ld [hl], $3e ; P
 ;	dec hl
 ;	add hl, de
 ;	dec c
-;	jr nz, .loop
+;	jr nz, .load_ap_loop
 ;	ret
 ;
+;Unused_PlaceEnemyHPLevel:
+;	push hl
+;	push hl
+;	ld hl, wPartyMonNicknames
+;	ld a, [wCurPartyMon]
+;	call GetNickname
+;	pop hl
+;	call PlaceString
+;	call CopyMonToTempMon
+;	pop hl
+;	ld a, [wCurPartySpecies]
+;	cp EGG
+;	jr z, .egg
+;	push hl
+;	ld bc, -12
+;	add hl, bc
+;	ld b, 0
+;	call DrawEnemyHP
+;	pop hl
+;	ld bc, 5
+;	add hl, bc
+;	push de
+;	call PrintLevel
+;	pop de
+;
+;.egg
+;	ret
+
 PlaceStatusString:
 ; Return nz if the status is not OK
 	push de
